6792c43dcfcac74fd3a276837a64d25006f08210
UnknownRoboto <59828127+UnknownRoboto@users.noreply.github.com>
2023-11-06 17:36:48 +0100

    Use abstract method handle Middleware class

--- Framework/Middleware.php
+++ Framework/Middleware.php
@@ -1,14 +1,32 @@
 <?php
 
 namespace Framework;
-use Framework\Framework;
 
-class Middleware{
+use Framework\MiddlewareEngine;
+use Framework\HTTPRequest;
 
-    public static function next($httpRequest,$httpResponse){  
-        return Framework::runMiddlewareChain($httpRequest,$httpResponse);
+use Illuminate\Http\Response;
+
+/* TODO: Maybe implements Middleware interface instead of abstract */
+
+abstract class Middleware {
+    /**
+     * The entry point method of middleware class
+     *
+     * @param HTTPRequest $httpRequest The current HTTP request of middleware 
+     * @param Response $httpResponse The current HTTP response of middleware 
+     * @return Response
+     */
+    abstract public function handle(HTTPRequest $httpRequest, Response $httpResponse): Response;
+
+    /**
+     * The next method, to pass on the next middleware in chain
+     *
+     * @param HTTPRequest $httpRequest The current HTTP request of middleware chain 
+     * @param Response $httpResponse The current HTTP response of middleware chain 
+     * @return Response
+     */
+    public static function next(HTTPRequest $httpRequest, Response $httpResponse): Response {  
+        return MiddlewareEngine::runNextMiddleware($httpRequest, $httpResponse);
     }
-
-
-
 }
